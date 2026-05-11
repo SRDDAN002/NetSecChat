564697478dfbf7f26ab05aeb745868e83c7d8df5
LiamDeSaldanha <129397980+LiamDeSaldanha@users.noreply.github.com>
2026-05-11 14:19:06 +0200

    seperated session msg from user msg

--- user_messages.py
+++ user_messages.py
@@ -10,63 +10,7 @@ import random
 
 # This will return an error saying "Session not found", because you're not logged in yet. That confirms the server is active.
 
-"""CONNECT_REQUEST"""
-def CONNECT_REQUEST():
-    return {
-        'request_type' : 1, 
-        'request_handle': random.randint(0, 2**32 - 1)
-        }
 
-
-"""CONNECT_RESPONSE-->REQUEST_TYPE = 24"""
-def CONNECT_RESPONSE(data):
-    session = data['session']
-    welcome = data['message']
-    username = data['username']
-    return session, welcome, username
-
-"""PING_REQUEST"""
-def PING_REQUEST(session):
-    return {
-        'request_type':3,
-        'session': session,
-        'request_handle': random.randint(0, 2**32 - 1)
-
-    }
-
-"""PING_RESPONSE-->REQUEST_TYPE = 23"""
-def PING_RESPONSE(data):
-    session = data['session']
-    #inlcude response handle?
-    return data
-
-"""DISCONNECT_REQUEST"""
-def DISCONNECT_REQUEST(session):
-    return {
-        'request_type' : 2,
-        'session' : session,
-        'request_handle': random.randint(0, 2**32 - 1)
-    }
-
-"""DISCONNECT_RESPONSE-->REPONSE TYPE = 23"""
-def DISCONNECT_RESPONSE(data):
-    message = data['message']
-    return message
-
-"""oOK RESPONSE-->RESPONSE TYPE = 21"""
-def OK_response():
-     #will do this later. seems a bit redundant now because every request has a response
-     return "OK from server"
-
-"""ERROR_REPONSE-->RESPONSE TYPE = 20"""
-def ERROR_response(data):
-     error = data['error']
-     return error
-
-"""SERVER_RESPONSE-->RESPONSE TYPE = 36"""
-def SERVER_message_RESPONSE(data):
-     server_mssg = data['message']
-     return server_mssg
 
 """SET_USERNAME_RESPONSE"""
 def SET_USERNAME_REQUEST(session, new_username):
